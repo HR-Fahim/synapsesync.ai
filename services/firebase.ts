d86ec8e88011e366abcf78c91e5c1bdd2e1b1ffe
Habibur Rahaman Fahim <66734379+HR-Fahim@users.noreply.github.com>
2025-12-14 06:17:36 +0600

    fix: Update Firebase configuration
    The Firebase configuration details, including API key, app ID, and measurement ID, have been updated. This ensures the application correctly connects to and utilizes Firebase services for authentication and analytics.

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5P3m4yf9fg2xoZyrImOBOWhaK70Bw4ZY",
+  apiKey: "AIzaSyCmAoOTRzEeZS6UBC9gKuP3GW5OkU-o7ec",
   authDomain: "synapse-sync-ai.firebaseapp.com",
   projectId: "synapse-sync-ai",
   storageBucket: "synapse-sync-ai.firebasestorage.app",
   messagingSenderId: "46476319012",
-  appId: "1:46476319012:web:5b201a5bfd8b0ed43f3bb1",
-  measurementId: "G-1H7N4YNE08"
+  appId: "1:46476319012:web:69fb0818084094b03f3bb1",
+  measurementId: "G-PSYE3GKDB2"
 };
 
 // Initialize Firebase
